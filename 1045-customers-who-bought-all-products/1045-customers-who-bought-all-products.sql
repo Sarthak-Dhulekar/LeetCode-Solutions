a1ad784217808008b8c8f8844ee59ab65989e788
Sarthak-Dhulekar <121718591+Sarthak-Dhulekar@users.noreply.github.com>
2026-08-21 21:14:21 +0530

    Time: 580 ms (73.96%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,11 +1,4 @@
-SELECT
-	customer_id
-FROM (SELECT
-      customer_id,
-	COUNT(DISTINCT product_key) AS product_key
+SELECT customer_id
 FROM Customer
-     GROUP BY customer_id) AS counting
-WHERE product_key IN
-(SELECT
-	COUNT(*) AS product_key
-FROM Product);
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
